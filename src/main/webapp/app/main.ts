15531a01a65ae68bb74e11273e533a61b4ff1d2f
Fabien Couillard <couillard.fabien+dev@gmail.com>
2023-01-12 12:26:06 +0100

    Apply vue-pinia module

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,9 +1,16 @@
 import { createApp } from 'vue';
 import { AppVue } from './common/primary/app';
 import router from './router/router';
+import { createPinia } from 'pinia';
+import piniaPersist from 'pinia-plugin-persist';
+
 // jhipster-needle-main-ts-import
 
 const app = createApp(AppVue);
 app.use(router);
+const pinia = createPinia();
+pinia.use(piniaPersist);
+app.use(pinia);
+
 // jhipster-needle-main-ts-provider
 app.mount('#app');
