0bd767d12ba39754f5e423408f146950ae538c1c
Fabien Couillard <couillard.fabien+dev@gmail.com>
2025-01-17 20:00:04 +0100

    Apply module: spring-boot-cucumber-mvc

---
+++ src/test/java/com/mycompany/myapp/cucumber/rest/HeaderAsserter.java
@@ -0,0 +1,9 @@
+package com.mycompany.myapp.cucumber.rest;
+
+public interface HeaderAsserter<T extends ResponseAsserter> {
+  HeaderAsserter<T> containing(String value);
+
+  HeaderAsserter<T> startingWith(String prefix);
+
+  T and();
+}
